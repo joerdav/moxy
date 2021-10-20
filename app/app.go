00336ddf0441e4c3343b4a2002533f2ae199626e
Joe-Davidson1802 <joe.davidson@infinityworks.com>
2021-10-20 16:21:38 +0100

    feat: make installable

--- app/app.go
+++ app/app.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	_ "embed"
 
 	"github.com/BurntSushi/toml"
 	"github.com/gorilla/mux"
@@ -12,14 +15,22 @@ import (
 	"github.com/joe-davidson1802/moxy/proxy"
 )
 
+//go:embed defaults.toml
+var cfile string
+
 func Run() {
 	var c config.Config
+	var cstring = cfile
 
-	file := "defaults.toml"
-
-	fmt.Println("Loading Defaults...")
-
-	if _, err := toml.DecodeFile(file, &c); err != nil {
+	if len(os.Args) >= 2 {
+		fmt.Printf("Loading config from %s\n", os.Args[1])
+		b, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		cstring = string(b)
+	}
+	if _, err := toml.Decode(cstring, &c); err != nil {
 		log.Fatal(err)
 	}
 
